cd1e049ac125d80e067396ae104f344affefb52e
Your Name <victor081196>
2022-06-17 17:48:03 -0500

    components

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ include_once 'config.php';
 
 // load controllers 
 require_once 'app/modules/appframework/appframework.controller.php';
+require_once 'app/modules/users/users.controller.php';
+
+
+
+
+// load models
+require_once 'app/modules/users/users.model.php';;
 
 
 $start = new AppFrameWorkController();
